f55061dd82e26e1642496e02cced808bb44e82fd
Matthias Reinarz <mreinarz@thoughtworks.com>
2017-02-12 21:39:10 +0000

    Colorful bash prompt

--- environments/production/site_modules/bash/files/defaults.sh
+++ environments/production/site_modules/bash/files/defaults.sh
@@ -22,6 +22,13 @@ shopt -s checkwinsize
   PS1="\[\e]0;\u@\h: \w\a\]$PS1"
 }
 
+# Colorful prompt
+if [[ ${EUID} == 0 ]] ; then
+  PS1='\[\033[01;31m\]\h\[\033[01;34m\] \W \$\[\033[00m\] '
+else
+  PS1='\[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\w \$\[\033[00m\] '
+fi
+
 # Some aliases
 alias :r='exec bash'
 alias :q='exit'
