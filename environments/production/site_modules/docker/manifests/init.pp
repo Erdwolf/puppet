e7bc0a937a525c7adc795d43ed217a57fc96ab84
Matthias Reinarz <mreinarz@thoughtworks.com>
2017-03-25 18:34:22 +0000

    Update docker repository

--- environments/production/site_modules/docker/manifests/init.pp
+++ environments/production/site_modules/docker/manifests/init.pp
@@ -1,8 +1,41 @@
 
 class docker {
+  include userhome
 
-  package  { 'docker.io':
+  package {[
+    'apt-transport-https',
+    'ca-certificates',
+    'curl',
+    'software-properties-common'
+  ]:
     ensure => present,
+  } ->
+
+  apt::key { 'docker':
+    source => 'https://download.docker.com/linux/ubuntu/gpg',
+    id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+  } ->
+
+  apt::source { 'download.docker.com':
+    location => 'https://download.docker.com/linux/ubuntu',
+    repos    => 'stable',
+  } ->
+
+  package { 'docker-ce':
+    ensure => present,
+  } ->
+
+  package { ['docker.io','docker-engine']:
+    ensure => absent,
+  } ->
+
+  group { 'docker':
+    ensure => 'present',
+  } ->
+
+  exec { "user in group":
+    unless => "/bin/grep -q 'docker\\S*$userhome::user' /etc/group",
+    command => "/usr/sbin/usermod -aG docker $userhome::user",
   }
 
 }
